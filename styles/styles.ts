1020e62fcdb4e755c4f24ff38a475107a3b54a40
Phuc <nguynphuc23@gmail.com>
2024-03-23 13:20:01 +0700

    first commit

---
+++ styles/styles.ts
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#3498db',
+  },
+  radius_box: {
+    display: 'flex',
+    backgroundColor: '#d9eaf8',
+    flexDirection: 'column',
+    width: 300,
+    borderRadius: 20,
+    padding: 20,
+  },
+  box_title: {
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {},
+});
